c75a28318c145e8adc88d47d34b036f10ef4537a
ZiTao-Li <zitao.l@alibaba-inc.com>
2024-04-22 17:42:29 +0800

    improve conversation.py

--- examples/conversation_basic/README.md
+++ examples/conversation_basic/README.md
@@ -1,12 +1,33 @@
 # Multi-Agent Conversation in AgentScope
 This is a demo of how to program a multi-agent conversation in AgentScope.
+
+In a nutshell, This example will show
+- How to initialize an application with different LLM model configurations;
+- How to use `DialogAgent` and `UserAgent`;
+- How to use `sequentialpipeline` to simplify the workflow.
+
 Complete code is in `conversation.py`, which set up a user agent and an
 assistant agent to have a conversation. When user input "exit", the
 conversation ends.
-You can modify the `sys_prompt` to change the role of assistant agent.
+You can modify the `sys_prompt` to change the persona of assistant agent.
 ```bash
 # Note: Set your api_key in conversation.py first
 python conversation.py
 ```
+
+
+## Tested Models
+
+These models are tested with this example.
+- [openai_chat](../model_configs_template/openai_chat_template.json)
+- [dashscope_chat](../model_configs_template/dashscope_chat_template.json)
+- [gemini_chat](../model_configs_template/gemini_chat_template.json)
+- [ollama_chat](../model_configs_template/ollama_chat_template.json)
+
+
+
 To set up model serving with open-source LLMs, follow the guidance in
 [scripts/REAMDE.md](../../scripts/README.md).
+
+## Prerequisites
+No additional package is required.
--- examples/conversation_basic/conversation.py
+++ examples/conversation_basic/conversation.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """A simple example for conversation between user and assistant agent."""
+import json
+import os
 import agentscope
 from agentscope.agents import DialogAgent
 from agentscope.agents.user_agent import UserAgent
@@ -9,32 +11,33 @@ from agentscope.pipelines.functional import sequentialpipeline
 def main() -> None:
     """A basic conversation demo"""
 
+    # replace the following JSON file with other available JSON files,
+    # or create your own model config to use other LLM models.
+    # E.g.,
+    # ../model_configs_template/gemini_chat_template.json
+    # ../model_configs_template/dashscope_chat_template.json
+    with open(
+        "../model_configs_template/openai_chat_template.json",
+        encoding="utf-8",
+    ) as f:
+        model_configs = json.load(f)
+
+    # you need to set the api_key as your API key
+    api_key = os.getenv("OPENAI_API_KEY")
+    for config in model_configs:
+        if "api_key" in config:
+            config["api_key"] = api_key
+
     agentscope.init(
-        model_configs=[
-            {
-                "model_type": "openai_chat",
-                "config_name": "gpt-3.5-turbo",
-                "model_name": "gpt-3.5-turbo",
-                "api_key": "xxx",  # Load from env if not provided
-                "organization": "xxx",  # Load from env if not provided
-                "generate_args": {
-                    "temperature": 0.5,
-                },
-            },
-            {
-                "model_type": "post_api_chat",
-                "config_name": "my_post_api",
-                "api_url": "https://xxx",
-                "headers": {},
-            },
-        ],
+        model_configs=model_configs,
     )
 
     # Init two agents
     dialog_agent = DialogAgent(
         name="Assistant",
         sys_prompt="You're a helpful assistant.",
-        model_config_name="gpt-3.5-turbo",  # replace by your model config name
+        # replace by your model config name
+        model_config_name="openai_chat_gpt-3.5-turbo",
     )
     user_agent = UserAgent()
 
